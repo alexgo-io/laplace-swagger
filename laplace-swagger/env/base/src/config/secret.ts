6dafef0b0b5b8221d7924192b5e06ee69dfc6a9b
hyossid <huvsid@gmail.com>
2022-05-10 14:42:31 +0900

    chore: fixed untracked files

--- laplace-swagger/env/base/src/config/secret.ts
+++
@@ -1,4 +0,0 @@
-export const X_HASURA_ADMIN_SECRET =
-  process.env['X_HASURA_ADMIN_SECRET'] ?? '{{X_HASURA_ADMIN_SECRET}}';
-export const HASURA_HOST = process.env['HASURA_HOST'] ?? '{{HASURA_HOST}}';
-export const HASURA_GRAPHQL_ENDPOINT = HASURA_HOST + '/v1/graphql';
